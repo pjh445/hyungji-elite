cbd24d4491e58b899a7228a39cf2daed40bdf27b
jeonghwa,park <pjh445@gmail.com>
2025-01-14 13:08:40 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,18 +4,21 @@ $(document).ready(()=> {
     const v1 = ()=>{
         $( "#visual section" ).eq(0).stop().animate({ opacity:0 }, 1).removeClass('view');
         $( "#visual section" ).eq(1).stop().addClass('view').animate({ opacity:1 }, 1, ()=>{
-            const visual = setInterval( v2 , 5000 );
+            let visual_1 = setInterval( v2 , 5000 );
+            clearInterval( visual_2 );            
         });
+        clearTimeout( visual );
     }
     const v2 = ()=>{
         $( "#visual section div" ).eq(0).addClass('small');
         $( "#visual section" ).eq(0).stop().animate({opacity:1}, 1).addClass('view');  
         $( "#visual section" ).eq(1).stop().removeClass('view').animate({ opacity:0 }, 1, ()=>{
-            const visual = setInterval( v1 , 5000 );
+            let visual_2 = setInterval( v1 , 5000 );
+            clearInterval(  visual_1 );
         });
     }
 
-    const visual = setTimeout( v1 , 5000 );
+    let visual = setTimeout( v1 , 5000 );
 
 
 });//...........js 끝부분
