7c55667713ded8a359efbdce685a96a7adde3dce
jeonghwa,park <pjh445@gmail.com>
2025-01-14 14:21:24 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
 $(document).ready(()=> {
 
     //visual부분
+    let visual_1;
+    let visual_2;
     const v1 = ()=>{
         $( "#visual section" ).eq(0).stop().animate({ opacity:0 }, 1).removeClass('view');
         $( "#visual section" ).eq(1).addClass('view').stop().animate({ opacity:1 }, 1, ()=>{
-            let visual_1 = setInterval( v2 , 7000 );
-            clearInterval( visual_2 );            
+            visual_1 = setInterval( v2 , 7000 );
+            clearInterval( visual_2 );
         });
         clearTimeout( visual );
     }
-    const v2 = ()=>{
+    const v2 = () =>{    
         $( "#visual section div" ).eq(0).addClass('small');
         $( "#visual section" ).eq(0).stop().animate({opacity:1}, 1).addClass('view');  
         $( "#visual section" ).eq(1).removeClass('view').stop().animate({ opacity:0 }, 1, ()=>{
-            let visual_2 = setInterval( v1 , 7000 );
+            visual_2 = setInterval( v1 , 7000 );
             clearInterval(  visual_1 );
         });
     }
