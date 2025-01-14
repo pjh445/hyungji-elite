88d7a0e0609e0aeb3167aed3bf5ce6c80af81f1c
jeonghwa,park <pjh445@gmail.com>
2025-01-14 13:14:15 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ $(document).ready(()=> {
     //visual부분
     const v1 = ()=>{
         $( "#visual section" ).eq(0).stop().animate({ opacity:0 }, 1).removeClass('view');
-        $( "#visual section" ).eq(1).stop().addClass('view').animate({ opacity:1 }, 1, ()=>{
-            let visual_1 = setInterval( v2 , 5000 );
+        $( "#visual section" ).eq(1).addClass('view').stop().animate({ opacity:1 }, 1, ()=>{
+            let visual_1 = setInterval( v2 , 7000 );
             clearInterval( visual_2 );            
         });
         clearTimeout( visual );
@@ -12,8 +12,8 @@ $(document).ready(()=> {
     const v2 = ()=>{
         $( "#visual section div" ).eq(0).addClass('small');
         $( "#visual section" ).eq(0).stop().animate({opacity:1}, 1).addClass('view');  
-        $( "#visual section" ).eq(1).stop().removeClass('view').animate({ opacity:0 }, 1, ()=>{
-            let visual_2 = setInterval( v1 , 5000 );
+        $( "#visual section" ).eq(1).removeClass('view').stop().animate({ opacity:0 }, 1, ()=>{
+            let visual_2 = setInterval( v1 , 7000 );
             clearInterval(  visual_1 );
         });
     }
